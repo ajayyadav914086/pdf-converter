cc81a8381e1a7d2798bb8d782505e29472f64375
Ajay yadav <ajay.yadav914086@gmail.com>
2022-12-24 13:43:18 +0530

    add remove"

--- app/src/main/java/com/androidmarket/pdfcreator/activities/HomeActivity.java
+++ app/src/main/java/com/androidmarket/pdfcreator/activities/HomeActivity.java
@@ -38,18 +38,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         setViews();
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-
-        AdsUtility.loadInterstitialAd(this);
         FrameLayout nativeAdFrameOne = findViewById(R.id.nativeAdFrameLayout);
         FrameLayout nativeAdFrameBack = findViewById(R.id.nativeAdFrameLayoutBack);
-        AdsUtility.loadNativeAd(this, nativeAdFrameOne);
-        AdsUtility.loadNativeAd(this, nativeAdFrameBack);
 
     }
 
@@ -77,160 +67,40 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.cardImageToPdf:
-
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                    mInterstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            AdsUtility.loadInterstitialAd(HomeActivity.this);
-                            Intent intent = new Intent(HomeActivity.this, SecondActivity.class);
-                            intent.putExtra("fragment", "imgToPdf");
-                            startActivity(intent);
-                        }
-                    });
-                } else {
-                    AdsUtility.loadInterstitialAd(HomeActivity.this);
                     Intent intent = new Intent(HomeActivity.this, SecondActivity.class);
                     intent.putExtra("fragment", "imgToPdf");
                     startActivity(intent);
-                }
-
 
                 break;
             case R.id.cardTextToPdf:
-
-
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                    mInterstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            AdsUtility.loadInterstitialAd(HomeActivity.this);
-                            Intent intent2 = new Intent(HomeActivity.this, SecondActivity.class);
-                            intent2.putExtra("fragment", "textToPdf");
-                            startActivity(intent2);
-                        }
-                    });
-                } else {
-                    AdsUtility.loadInterstitialAd(HomeActivity.this);
                     Intent intent2 = new Intent(HomeActivity.this, SecondActivity.class);
                     intent2.putExtra("fragment", "textToPdf");
                     startActivity(intent2);
-                }
-
                 break;
             case R.id.cardQrToPdf:
-
-
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                    mInterstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            AdsUtility.loadInterstitialAd(HomeActivity.this);
-
-                            Intent intent3 = new Intent(HomeActivity.this, SecondActivity.class);
-                            intent3.putExtra("fragment", "qrToPdf");
-                            startActivity(intent3);
-                        }
-                    });
-                } else {
-                    AdsUtility.loadInterstitialAd(HomeActivity.this);
-
                     Intent intent3 = new Intent(HomeActivity.this, SecondActivity.class);
                     intent3.putExtra("fragment", "qrToPdf");
                     startActivity(intent3);
-                }
-
                 break;
             case R.id.cardExcelToPdf:
-
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                    mInterstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            AdsUtility.loadInterstitialAd(HomeActivity.this);
-                            Intent intent4 = new Intent(HomeActivity.this, SecondActivity.class);
-                            intent4.putExtra("fragment", "excelToPdf");
-                            startActivity(intent4);
-                        }
-                    });
-                } else {
-                    AdsUtility.loadInterstitialAd(HomeActivity.this);
                     Intent intent4 = new Intent(HomeActivity.this, SecondActivity.class);
                     intent4.putExtra("fragment", "excelToPdf");
                     startActivity(intent4);
-                }
-
-
                 break;
             case R.id.cardAddWatermark:
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                    mInterstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            AdsUtility.loadInterstitialAd(HomeActivity.this);
-                            Intent intent5 = new Intent(HomeActivity.this, SecondActivity.class);
-                            intent5.putExtra("fragment", "watermark");
-                            startActivity(intent5);
-
-                        }
-                    });
-                } else {
-                    AdsUtility.loadInterstitialAd(HomeActivity.this);
-                    Intent intent5 = new Intent(HomeActivity.this, SecondActivity.class);
+                Intent intent5 = new Intent(HomeActivity.this, SecondActivity.class);
                     intent5.putExtra("fragment", "watermark");
                     startActivity(intent5);
-
-                }
-
-
                 break;
             case R.id.cardHistory:
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                    mInterstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            AdsUtility.loadInterstitialAd(HomeActivity.this);
-                            Intent intent6 = new Intent(HomeActivity.this, SecondActivity.class);
-                            intent6.putExtra("fragment", "history");
-                            startActivity(intent6);
-                        }
-                    });
-                } else {
-                    AdsUtility.loadInterstitialAd(HomeActivity.this);
-                    Intent intent6 = new Intent(HomeActivity.this, SecondActivity.class);
+                Intent intent6 = new Intent(HomeActivity.this, SecondActivity.class);
                     intent6.putExtra("fragment", "history");
                     startActivity(intent6);
-                }
-
-
                 break;
             case R.id.cardViewFiles:
-
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                    mInterstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            AdsUtility.loadInterstitialAd(HomeActivity.this);
-                            Intent intent7 = new Intent(HomeActivity.this, SecondActivity.class);
-                            intent7.putExtra("fragment", "view");
-                            startActivity(intent7);
-                        }
-                    });
-                } else {
-                    AdsUtility.loadInterstitialAd(HomeActivity.this);
                     Intent intent7 = new Intent(HomeActivity.this, SecondActivity.class);
                     intent7.putExtra("fragment", "view");
                     startActivity(intent7);
-                }
-
-
                 break;
             case R.id.cardSettings:
                 Intent intent8 = new Intent(HomeActivity.this, SecondActivity.class);
@@ -243,55 +113,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onBackPressed() {
-        ShowBackStuff();
-    }
-    boolean doubleBackToExitPressedOnce = false;
 
-    public void ShowBackStuff() {
-        if (mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
-            mInterstitialAd.setAdListener(new AdListener() {
-                @Override
-                public void onAdClosed() {
-                    if (doubleBackToExitPressedOnce) {
-                        finish();
-                        return;
-                    }
-
-                    doubleBackToExitPressedOnce = true;
-                    RelativeLayout exitLayout = findViewById(R.id.exitLayout);
-                    exitLayout.setVisibility(View.VISIBLE);
-                    new Handler().postDelayed(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            doubleBackToExitPressedOnce = false;
-                        }
-                    }, 2000);
-                }
-            });
-        } else {
-            if (doubleBackToExitPressedOnce) {
-                finish();
-                return;
-            }
-
-            doubleBackToExitPressedOnce = true;
-            RelativeLayout exitLayout = findViewById(R.id.exitLayout);
-            exitLayout.setVisibility(View.VISIBLE);
-            new Handler().postDelayed(new Runnable() {
-
-                @Override
-                public void run() {
-                    doubleBackToExitPressedOnce = false;
-                }
-            }, 2000);
-        }
-    }
-
-
-        public void Disappear(View view) {
-        RelativeLayout rel = findViewById(R.id.exitLayout);
-        rel.setVisibility(View.GONE);
     }
 }
